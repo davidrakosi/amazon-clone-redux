e0a459035aa1923d00917e228ec9d10d91b9cdf7
David Rakosi <61084415+drakosi99@users.noreply.github.com>
2021-01-04 14:54:13 -0800

    ADD: 'deliver to' part

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,6 +9,12 @@ const Header = () => {
                 <img className="header__logo" src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
                      alt='Amazon Logo'/>
             </Link>
+
+            <div className='header__option'>
+                <span className='header__optionLineOne'>Deliver to</span>
+                <span className='header__optionLineTwo'>Los Angeles</span>
+            </div>
+            
         </div>
     )
 }
