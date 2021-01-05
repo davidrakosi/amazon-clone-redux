378213ee9be676e187524b69f49f3b1a929e3a45
David Rakosi <61084415+drakosi99@users.noreply.github.com>
2021-01-04 16:48:56 -0800

    ADD: header belt
    MTC: reset class namings

--- src/components/Header.js
+++ src/components/Header.js
@@ -34,62 +34,70 @@ const Header = () => {
     }
     return (
         <div className='header'>
-            <Link to="/">
-                <img className="header__logo" src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
-                     alt='Amazon Logo'/>
-            </Link>
-
-            <div className='header__option'>
-                <span className='header__optionLineOne'>Deliver to</span>
-                <span className='header__optionLineTwo'>Los Angeles</span>
-            </div>
-
-            <div className='header__search'>
-                <input type='text' className='header__searchInput'/>
-                <SearchIcon className="header__searchIcon"/>
-            </div>
-
-            <div className='header__nav'>
-
-                <Link to='/login'>
-                    <div className='header__option' onClick={handleAuth}>
-                        {
-                            userName ? (
-                                <>
-                                    <span className='header__optionLineOne'>Hello {userName}</span>
-                                    <span className='header__optionLineTwo'>Sign Out</span>
-                                </>
-                            ) : (
-                                <>
-                                    <span className='header__optionLineOne'>Guest</span>
-                                    <span className='header__optionLineTwo'>Sign In</span>
-                                </>
-                            )
-                        }
-                    </div>
+            <div className='header__main'>
+                <Link to="/">
+                    <img className="header__logo" src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+                         alt='Amazon Logo'/>
                 </Link>
 
-                <Link to='/orders'>
-                    <div className="header__option">
-                        <span className="header__optionLineOne">Returns</span>
-                        <span className="header__optionLineTwo">& Orders</span>
-                    </div>
-                </Link>
-
-                <div className="header__option">
-                    <span className="header__optionLineOne">Your</span>
-                    <span className="header__optionLineTwo">Prime</span>
+                <div className='header__option'>
+                    <span className='header__optionLineOne'>Deliver to</span>
+                    <span className='header__optionLineTwo'>Los Angeles</span>
                 </div>
 
-                <Link to="/checkout">
-                    <div className="header__optionBasket">
-                        <ShoppingBasketIcon/>
-                        <span
-                            className="header__optionLineTwo header__basketCount">n/a</span> {/*number of items in cart to be added later*/}
-                    </div>
-                </Link>
+                <div className='header__search'>
+                    <input type='text' className='header__searchInput'/>
+                    <SearchIcon className="header__searchIcon"/>
+                </div>
 
+                <div className='header__nav'>
+
+                    <Link to='/login'>
+                        <div className='header__option' onClick={handleAuth}>
+                            {
+                                userName ? (
+                                    <>
+                                        <span className='header__optionLineOne'>Hello {userName}</span>
+                                        <span className='header__optionLineTwo'>Sign Out</span>
+                                    </>
+                                ) : (
+                                    <>
+                                        <span className='header__optionLineOne'>Guest</span>
+                                        <span className='header__optionLineTwo'>Sign In</span>
+                                    </>
+                                )
+                            }
+                        </div>
+                    </Link>
+
+                    <Link to='/orders'>
+                        <div className="header__option">
+                            <span className="header__optionLineOne">Returns</span>
+                            <span className="header__optionLineTwo">& Orders</span>
+                        </div>
+                    </Link>
+
+                    <div className="header__option">
+                        <span className="header__optionLineOne">Your</span>
+                        <span className="header__optionLineTwo">Prime</span>
+                    </div>
+
+                    <Link to="/checkout">
+                        <div className="header__optionBasket">
+                            <ShoppingBasketIcon/>
+                            <span
+                                className="header__optionLineTwo header__basketCount">n/a</span> {/*number of items in cart to be added later*/}
+                        </div>
+                    </Link>
+
+                </div>
             </div>
+
+            <div className='header__belt'>
+                
+            </div>
+
+
         </div>
     )
 }
