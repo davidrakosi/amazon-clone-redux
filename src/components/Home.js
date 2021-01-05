48a515c971f9c94f788ebe7ddcdc36b483f35b0d
David Rakosi <61084415+drakosi99@users.noreply.github.com>
2021-01-05 10:51:29 -0800

    ADD: product component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,8 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import './Home.css'
 
 import db from '../firebase'
+import Product from "./Product";
 
 const Home = () => {
     const [products, setProducts] = useState([])
@@ -23,14 +24,40 @@ const Home = () => {
         <div className='home'>
             <div className='home__container'>
                 <div className="home__banner"
-                     style={{
-                         backgroundImage: 'url(https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Events/2021/NYNY/Fuji_TallHero_NYNY_en_US_1x._CB412256579_.jpg)'
-                     }}
+                    style={{
+                        backgroundImage: 'url(https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Events/2021/NYNY/Fuji_TallHero_NYNY_en_US_1x._CB412256579_.jpg)'
+                    }}
                 >
                 </div>
 
                 <div className='home__main'>
-                    
+                    <div className='home__row'>
+                        {
+                            products.slice(0, 2).map(({ id, item }) => (
+                                <Product
+                                    id={id}
+                                    title={item.name}
+                                    price={item.price}
+                                    rating={item.rating}
+                                    image={item.image}
+                                />
+                            ))
+                        }
+                    </div>
+
+                    <div className='home__row'>
+                        {
+                            products.slice(2, products.length).map(({ id, item }) => (
+                                <Product
+                                    id={id}
+                                    title={item.name}
+                                    price={item.price}
+                                    rating={item.rating}
+                                    image={item.image}
+                                />
+                            ))
+                        }
+                    </div>
                 </div>
             </div>
         </div>
---
+++ src/components/Product.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Product.css'
+
+const Product = ({ id, title, price, rating, image }) => {
+    
+    return (
+        <div className='product'>
+           
+        </div>
+    )
+}
+
+
+export default Product
