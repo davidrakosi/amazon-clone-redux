04bb5a92142f3d59f830e576ca9abd57b8b47e2e
David Rakosi <61084415+drakosi99@users.noreply.github.com>
2021-01-05 10:52:26 -0800

    ADD: product component structure

--- src/components/Product.js
+++ src/components/Product.js
@@ -2,10 +2,28 @@ import React from 'react'
 import './Product.css'
 
 const Product = ({ id, title, price, rating, image }) => {
-    
+    const addToCart = () => {
+
+    }
     return (
         <div className='product'>
-           
+            <div className="product__info">
+
+                <span>{title}</span>
+                <span className="product__price">${price}</span>
+
+                <div className="product__rating">
+                    {
+                        Array(rating).fill().map((_, i) => (
+                            <p>🌟</p>
+                        ))
+                    }
+                </div>
+
+                <img src={image} alt={title} />
+
+                <button onClick={addToCart}>Add to Cart</button>
+            </div>
         </div>
     )
 }
