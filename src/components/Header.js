1ea9e6309d17e20b334d4bfb0cec306d2ed8e92a
David Rakosi <61084415+drakosi99@users.noreply.github.com>
2021-01-04 16:32:06 -0800

    ADD: your prime button
    FIX: indentation

--- src/components/Header.js
+++ src/components/Header.js
@@ -65,14 +65,19 @@ const Header = () => {
                             )
                         }
                     </div>
-
-                    <Link to='/orders'>
-                        <div className="header__option">
-                            <span className="header__optionLineOne">Returns</span>
-                            <span className="header__optionLineTwo">& Orders</span>
-                        </div>
-                    </Link>
                 </Link>
+
+                <Link to='/orders'>
+                    <div className="header__option">
+                        <span className="header__optionLineOne">Returns</span>
+                        <span className="header__optionLineTwo">& Orders</span>
+                    </div>
+                </Link>
+
+                <div className="header__option">
+                    <span className="header__optionLineOne">Your</span>
+                    <span className="header__optionLineTwo">Prime</span>
+                </div>
 
             </div>
         </div>
