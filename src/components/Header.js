9cf4a6e6eae7a04823d4b6cd64b43ee872de9495
David Rakosi <61084415+drakosi99@users.noreply.github.com>
2021-01-04 16:31:05 -0800

    ADD: returns & orders button

--- src/components/Header.js
+++ src/components/Header.js
@@ -65,6 +65,13 @@ const Header = () => {
                             )
                         }
                     </div>
+
+                    <Link to='/orders'>
+                        <div className="header__option">
+                            <span className="header__optionLineOne">Returns</span>
+                            <span className="header__optionLineTwo">& Orders</span>
+                        </div>
+                    </Link>
                 </Link>
 
             </div>
