09313d1b8e64b7f5b0e429bcd27b517b60028e88
David Rakosi <61084415+drakosi99@users.noreply.github.com>
2021-01-05 18:37:42 -0800

    ADD: cart ad image

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -3,7 +3,12 @@ import React from 'react'
 const Cart = () => {
     return (
         <div className='cart'>
-            
+            <img
+                className="cart__ad"
+                src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
+                alt="Cart Top Ad"
+            />
+
         </div>
     )
 }
