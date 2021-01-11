1c350e00baa15f08dc589cadcd40a595219e008c
David Rakosi <61084415+drakosi99@users.noreply.github.com>
2021-01-10 17:15:55 -0800

    ADD: calculate subtotal

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,10 +1,27 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
+
+
+import { useDispatch } from 'react-redux'
 
 import { useSelector } from 'react-redux'
-import { selectCartItemsAmount } from '../features/cart/cartSlice'
+import { selectCartItems } from '../features/cart/cartSlice'
 
 const Cart = () => {
-    const cartItemsCount = useSelector(selectCartItemsAmount)
+    const [totalPrice, setTotalPrice] = useState(0)
+    const dispatch = useDispatch()
+
+    const cartItems = useSelector(selectCartItems)
+
+    useEffect(() => {
+        let priceTotal = 0
+
+        cartItems.map(item => {
+            console.log(item)
+            priceTotal += (item.price * item.quantity)
+        })
+
+        setTotalPrice(priceTotal)
+    }, [])
 
     return (
         <div className='cart'>
@@ -13,7 +30,7 @@ const Cart = () => {
             </div>
 
             <div className="cart__total">
-                <h2>Subtotal ({cartItemsCount} items):${ }</h2>
+                <h2>Subtotal ({cartItems.length} items):${totalPrice}</h2>
                 <div className="cart__totalGiftWrap">
                     <input type="checkbox" /> {/* add wrap option */}
                     <p> This order contains a gift</p>
